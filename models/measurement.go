163be461e0422913babf8921d92fbfd7c4198105
Oladele Seyi <t.seyi@buycoins.africa>
2024-09-09 01:19:21 +0100

    fix uuid

--- models/measurement.go
+++ models/measurement.go
@@ -1,13 +1,14 @@
 package models
 
 import (
+	uuid "github.com/satori/go.uuid"
 	"gorm.io/datatypes"
 )
 
 type Measurement struct {
 	Base
-	UserID       uint   `gorm:"not null"` // Foreign key reference to User
-	Name         string `gorm:"not null"`
+	UserID       uuid.UUID `gorm:"not null"` // Foreign key reference to User
+	Name         string    `gorm:"not null"`
 	MeasuredBy   *string
 	Measurements datatypes.JSON `gorm:"type:jsonb"` // PostgreSQL JSONB type
 	ShoeSize     *string
